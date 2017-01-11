4156178047429199ef3ea2884f9197a851c997fd
Máté Szurovecz <szuri92@gmail.com>
2017-01-11 11:39:43 +0100

    add server

---
+++ server/server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var mysql = require("mysql");
+var express = require('express');
+var bodyParser = require('body-parser');
+var vm = require('./validator.js');
+
+var app = express();
+app.use(bodyParser.json());
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "'root'",
+  password: "cerna2",
+  database: "vizsga"
+});
+
+app.use(express.static('../client'));
+
+app.post('/exam', function(req, res) {
+  if (vm.validator(req.body) == false) {
+    console.log(req.body);
+    res.send( {"status": "error", "message": "thank you"})
+    console.log('nem jo')
+  } else if (vm.validator(req.body) == true) {
+    console.log('jo');
+    con.query('SELECT project_name FROM projects;', function(err, rows){
+      var allRow = rows.map(function(rows){
+        return rows.project_name
+      });
+      var response = {"status": "ok","projects": allRow }
+      console.log(rows);
+      res.send(response);
+    });
+  }
+});
+
+app.listen(3000);
