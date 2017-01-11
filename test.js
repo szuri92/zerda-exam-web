404584cf5c33f394777e00d1b04237917459d5b3
Máté Szurovecz <szuri92@gmail.com>
2017-01-11 11:54:28 +0100

    add 3 more test

--- test.js
+++ test.js
@@ -23,3 +23,24 @@ test("validator rejects valid email, a scale of 9 and positive feedback", functi
     t.equal(actual, expected);
     t.end();
 });
+
+test("validator rejects valid email, a scale of 15 and negative feedback", function(t) {
+  var actual = vm.validator({feedback: 'It is hard so far', scale: '15', email: 'valid@email'});
+  var expected = false;
+    t.equal(actual, expected);
+    t.end();
+});
+
+ test("validator rejects valid email, a scale of negative and positive feedback", function(t) {
+  var actual = vm.validator({feedback: 'amazing awesome smart student so good', scale: '-2', email: 'valid@email'});
+  var expected = false;
+    t.equal(actual, expected);
+    t.end();
+});
+
+test("validator accept valid email, a scale of valid number as int and positive feedback", function(t) {
+  var actual = vm.validator({feedback: 'amazing awesome smart student so good', scale: 15, email: 'valid@email'});
+  var expected = true;
+    t.equal(actual, expected);
+    t.end();
+});
