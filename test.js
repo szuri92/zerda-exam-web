a7c63b16981f4404b720637ad0c3ebf4d223e2f6
Máté Szurovecz <szuri92@gmail.com>
2017-01-11 10:19:20 +0100

    testing done

--- test.js
+++ test.js
@@ -1,7 +1,25 @@
 'use strict';
 
+var vm = require('./server/validator.js')
 var test = require('tape');
 
-test("validator accepts valid email, a scale of 15 and positive feedback");
-test("validator rejects unfilled email, a scale of 15 and positive feedback");
-test("validator rejects valid email, a scale of 9 and positive feedback");
+test("validator accepts valid email, a scale of 15 and positive feedback", function(t) {
+  var actual = vm.validator({feedback: 'amazing awesome smart student so good', scale: 15, email: 'valid@email'});
+  var expected = true;
+    t.equal(actual, expected);
+    t.end();
+});
+
+test("validator rejects unfilled email, a scale of 15 and positive feedback", function(t) {
+  var actual = vm.validator({feedback: 'amazing awesome smart student so good', scale: 15, email: 'notvalid'});
+  var expected = false;
+    t.equal(actual, expected);
+    t.end();
+});
+
+test("validator rejects valid email, a scale of 9 and positive feedback", function(t) {
+  var actual = vm.validator({feedback: 'amazing awesome smart student so good', scale: 9, email: 'valid@email'});
+  var expected = false;
+    t.equal(actual, expected);
+    t.end();
+});
